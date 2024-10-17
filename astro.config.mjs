1a2a9cd7fd305099aa025359c1310748e1ecba2a
HBH786 <121463528+HBH786@users.noreply.github.com>
2024-10-17 18:25:27 +0100

    Add files via upload
    redirects fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,9 @@
 import { defineConfig } from 'astro/config';
-import pagefind from "astro-pagefind"
+import pagefind from "astro-pagefind";
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
-
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
-  integrations: [tailwind(), pagefind(), sitemap()],
-
-  output: "hybrid",
-  site: 'https://your-domain.com',
-
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-      wrap: true
-    }
-  },
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [tailwind(), pagefind()],
+  output: "static",
+  site: 'https://your-domain.com', // Replace with your actual domain
 });
